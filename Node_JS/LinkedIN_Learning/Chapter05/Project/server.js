36c5312be0022a38e5d15b5c052dab3bcc83ab35
unknown <prakharkumar506978@gmail.com>
2021-03-18 23:34:09 +0530

    18-03-2021

---
+++ Node_JS/LinkedIN_Learning/Chapter05/Project/server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname));
+
+let server = app.listen(3000, () => {
+	console.log("Server is listening on port ", server.address().port);
+});
+
