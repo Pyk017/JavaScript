d2eb22025ae1a2f83f90194f297556de5a653ae3
unknown <prakharkumar506978@gmail.com>
2021-05-14 12:27:00 +0530

    14-05-2021

--- Note Taker App/script.js
+++ Note Taker App/script.js
@@ -10,24 +10,42 @@ function main() {
     }
 
     let output = document.getElementById('notes');
-    
-    let newDiv = document.createElement('div');
-    newDiv.setAttribute('class', 'col-5 mt-3 py-5');
 
-    newDiv.innerHTML = "<h5>Notes: " + count +"</h5>"
+    let newDiv = document.createElement('div');
+    newDiv.setAttribute('class', 'col-5 mt-3');
+    newDiv.style.padding = "5px";
+    newDiv.style.border = "1px solid grey";
+    newDiv.style.borderRadius = "5px";
+
+    newDiv.innerHTML = "<h5>Notes: " + count + "</h5>"
+
+    let divNew = document.createElement('div');
+
+    let para = document.createElement('p');
+    para.style.width = "250px";
+    para.style.whiteSpace = 'nowrap';
+    para.style.overflow = "hidden";
+    para.style.textOverflow = "ellipsis";
+
 
     let element = document.createTextNode(text);
 
+    para.appendChild(element);
+
     let button = document.createElement('input');
     button.setAttribute('class', "btn btn-primary");
+    button.setAttribute('data-bs-toggle', "modal");
+    button.setAttribute('data-bs-target', "#modalID");
     button.type = "button";
-    button.value = "view details";
-    button.name="view details";
+    button.value = "View";
+    button.name = "view details";
     button.style.height = "50px";
-    button.style.width = "100px";
+    button.style.width = "80px";
+    button.onclick = function() {
+        createModal(text, count);
+    }
 
-
-    newDiv.appendChild(element);
+    newDiv.appendChild(para);
 
     newDiv.appendChild(button);
 
@@ -44,3 +62,11 @@ function closeBtn(r){
 function clearTextArea(){
     document.getElementById('textarea').value = '';
 }
+
+function createModal(content, count) {
+    document.getElementById('staticBackdropLabel').innerHTML = "Note " + count + ":";
+    let contentDiv = document.getElementById("modalContent");
+    let text = document.createTextNode(content);
+    contentDiv.appendChild(text);
+}
+
