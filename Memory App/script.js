28a6ca4bc04a89cf9131a95ebf0c4d2b891149ca
unknown <prakharkumar506978@gmail.com>
2021-05-16 09:41:18 +0530

    16-05-2021

--- Memory App/script.js
+++ Memory App/script.js
@@ -1,3 +1,11 @@
-// document.getElementsByClassName('cell').addEventListener('click', () => {
-//     alert("this is a click!");
-// })
+function main() {
+    const cell = document.getElementsByClassName("card");
+
+    const num = cell.length;
+
+    for (let i = 0; i<num; i++) {
+        cell[i].addEventListener('click', () => {
+            cell[i].classList.toggle('flipCard')       
+        })
+    }
+}
