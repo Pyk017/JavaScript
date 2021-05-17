2d82527ddb3bd1ad0cc8a30994f7cad3a9caec60
unknown <prakharkumar506978@gmail.com>
2021-05-17 21:33:03 +0530

    17-05-2021

--- Memory App/script.js
+++ Memory App/script.js
@@ -1,43 +1,59 @@
-let images = {};
+let imageArray = [];
+
+function createArray() {
+    for (let i = 0; i < 9; i++) {
+        imageArray.push(createImg(i));
+    }
+}
+
+function shuffleArray(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex != 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue
+    }
+}
+
 
 function createImg (i) {
     let img = document.createElement('img');
-    img.src = `/images/img${i}.jpg`;
+    img.src = `/images/img${i + 1}.jpg`;
     img.width = '110';
     img.height = '110';
-    img.setAttribute('id', 'img' + i);
-    images[i] = img;
+    img.setAttribute('id', `img${i + 1}`);
+    imageArray[i] = img;
+    return img;
 }
 
-function inputImages () {
-    const cell = document.getElementsByClassName("card");
-    
-    for(let i = 1; i < 10; i++) {
-        createImg(i);
-    }
-
-    console.log(images);
-}
+let message;
 
 function main() {
-    const cell = document.getElementsByClassName("card");
-
-    const num = cell.length;
-    let random;
-    inputImages();
+    createArray();
+    console.log(imageArray);
+    shuffleArray(imageArray);
+    console.log(imageArray);
+    // const cell = document.getElementsByClassName("card");
+    // let random;
     
-    for (let i = 0; i<9; i++) {
-        cell[i].addEventListener('click', () => { 
-            cell[i].classList.toggle('flipCard');
-            random = Math.floor((Math.random() * 9) + 1);
-            cell[i].childNodes[3].appendChild(images[random]);
-            // cell[i].childNodes[3].appendChild();
-            invoked(images[random]);
-            cell[i].childNodes[3].childNodes.
+    // for (let i = 0; i<9; i++) {
+    //     cell[i].addEventListener('click', () => { 
+    //         cell[i].classList.toggle('flipCard');
+    //         random = Math.floor((Math.random() * 9) + 1);
+    //         let createImage = createImg(random);
+    //         // cell[i].childNodes[3].appendChild(images[random]);
+    //         cell[i].childNodes[3].appendChild(createImage);
+    //         // cell[i].childNodes[3].appendChild();
+    //         invoked(createImage);
+    //         // console.log(cell[i].childNodes[3].childNodes);
+    //         // console.log(checker1, checker2);
+    //     })
 
-        })
-    }
-
+    // }
+    // checkingImages();
 
 }
 
@@ -48,14 +64,39 @@ function invoked(img) {
     if (count == 2) {
         checker2 = img;
         count = 0;
-        checking();
+        checkingImages();
+        return ;
     }
     checker1 = img;
 }
 
+let score = 0;
+let failed = 0 ;
 
-function checking() {
-    console.log(checker1, checker2);
+
+function checkingImages() {
+    console.log(checker1.id, checker2.id);
+    
+    if (checker1.id == checker2.id) {
+        
+        // alert("Success!");
+        message = "Success!";
+        let success = document.getElementById('totalScore');
+        score += 1;
+        success.innerHTML = score;
+
+    } else {
+
+        // alert("Failed!");
+        message = "Failed!";
+        let fail = document.getElementById('failedAttempt');
+        failed += 1;
+        fail.innerHTML = failed;
+
+    }
+
+
 }
+
 
 
